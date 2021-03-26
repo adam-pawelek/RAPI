dd830f0cdcfef7acb5872bbb53d0f0c792ff0b37
TabSchu <t.drieschner@me.com>
2021-03-26 16:29:33 +0200

    add credentials to config file

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   jwt_secret: 'sshhh',
-  database_host: '',
+  database_host: '195.148.22.201',
   database_port: 5430,
-  database_user: '',
-  database_pass: '',
-  database_db: '',
+  database_user: 'monkey_user',
+  database_pass: 'monkey_pass',
+  database_db: 'monkey_db',
   bucketname: 'imager'
 }
