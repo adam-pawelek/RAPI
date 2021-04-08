46bb794422a693d09cf71f4845b4e155ad24daa2
TabSchu <t.drieschner@me.com>
2021-04-08 08:32:17 +0300

    ci-pipeline: add docker-file and gitlab-ci-yml

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  jwt_secret: 'sshhh',
-  database_host: '195.148.22.201',
-  database_port: 5430,
-  database_user: 'monkey_user',
-  database_pass: 'monkey_pass',
-  database_db: 'monkey_db',
-  bucketname: 'imager'
+  jwt_secret: process.env.JWT_SECRET || 'sshhh',
+  database_host:  process.env.DB_HOST || '195.148.22.201',
+  database_port: process.env.DB_PORT || 5430,
+  database_user:  process.env.DB_USER || 'monkey_user', // Todo: Why did he delete the credentials? should we too?
+  database_pass: process.env.DB_PASS || 'monkey_pass',
+  database_db: process.env.DB_DB || 'monkey_db',
+  bucketname: process.env.BUCKET || 'imager'
 }
+
