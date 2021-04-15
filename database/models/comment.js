589b04cda200af4ad661a188d477c5ecebcaa107
AA9376 <aa9376@student.jamk.fi>
2021-04-15 17:16:21 +0500

    Created model for comments and added association between model and Image

---
+++ database/models/comment.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+
+module.exports = function (database) {
+  return database.define('comment', {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4
+    },
+    comment: {
+      type: Sequelize.TEXT
+    },
+    image_id: {
+      type: Sequelize.UUID
+    }
+  })
+}
