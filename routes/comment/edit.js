0b68e8c0528391b5b872bb94ba3aa023930109f8
AA9376 <aa9376@student.jamk.fi>
2021-04-18 21:50:58 +0500

    Fixed editing comments for user & admin

--- routes/comment/edit.js
+++ routes/comment/edit.js
@@ -5,16 +5,37 @@ module.exports = [
     //Changes comment text for one comment to another text from payload
     method: 'PUT',
     path: '/comment/{commentId}',
+    options: {
+      auth: {
+        scope: ['admin', 'user']
+      }
+    },
     handler: async function (request, h) {
-      await Comment.update(
-        { comment: request.payload.comment },
-        { where: { id: request.params.commentId } }
+
+      let updated = null
+
+      if (request.auth.credentials.model.dataValues.scope === 'admin') {
+        updated = await Comment.update(
+          { comment: request.payload.comment },
+          { where: { id: request.params.commentId } }
         );
-      return Comment.findOne({
-        where: {
-          id: request.params.commentId
-        }
-      })
+      }
+      if (request.auth.credentials.model.dataValues.scope === 'user') {
+        const userId = request.auth.credentials.user.id
+        const comment = Comment.findOne({ where: { id: request.params.commentId } })
+        if (comment.userId === userId)
+          updated = await Comment.update(
+            { comment: request.payload.comment },
+            { where: { id: request.params.commentId } }
+          )
+      }
+      if (updated) {
+        return Comment.findOne({
+          where: {
+            id: request.params.commentId
+          }
+        })
+      } else throw new Error('Comment not found or it doesn\'t belong to you')
     }
   }
 ]
