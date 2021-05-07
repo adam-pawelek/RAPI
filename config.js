53d6f7708de4704f657e46a21ecfad5f0ded449a
Vanessa Skowronsky <vskowronsky@stud.hs-bremen.de>
2021-05-07 20:11:39 +0200

    Delete duplicated code

--- config.js
+++ config.js
@@ -17,7 +17,5 @@ module.exports = {
   github_secret: 'UwtWxrAszEB32RrQJ52byASWVxfxq9sP',
   redis_host: '195.148.22.201',
   redis_port: 6379,
-  graylog_host: '195.148.22.201',
-  graylog_port: 12201
 }
 
