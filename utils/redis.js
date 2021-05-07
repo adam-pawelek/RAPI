3d23cd61b84e84ea6bcb1c6e9e95cdacaa8bdc7b
Vanessa Skowronsky <vskowronsky@stud.hs-bremen.de>
2021-05-07 16:16:40 +0200

    Renamed variable

--- utils/redis.js
+++ utils/redis.js
@@ -2,7 +2,7 @@ const redis = require('redis')
 const asyncRedis = require('async-redis')
 const config = require('../config')
 
-syncRedisClient = redis.createClient(config.redis_port, config.redis_address)
+syncRedisClient = redis.createClient(config.redis_port, config.redis_host)
 redisClient = asyncRedis.decorate(syncRedisClient)
 
 module.exports = {
