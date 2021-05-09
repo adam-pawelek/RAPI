a3b0546d6a91cf28ffb671f78db1221b5fef71cd
Vanessa Skowronsky <vskowronsky@stud.hs-bremen.de>
2021-05-09 22:28:45 +0200

    Fix in me.js

--- routes/user/me.js
+++ routes/user/me.js
@@ -1,3 +1,5 @@
+const { User } = require('../../database')
+const Boom = require('@hapi/boom')
 
 module.exports = [
   {
@@ -8,12 +10,14 @@ module.exports = [
         scope: ['admin', 'user']
       },
       handler: async function (request, h) {
-
         let user = request.auth.credentials.user
+        console.log(user.id);
 
-        console.log(user.name);
-
-        const me = request.auth.credentials.model
+        const me = await User.findAll({
+          where: {
+            id: user.id
+          }
+        })
 
         return {
           msg: 'Success! This is ME',
@@ -30,15 +34,22 @@ module.exports = [
         scope: ['admin', 'user']
       },
       handler: async function (request, h) {
+try{
+  const me = request.auth.credentials.model
 
-        const me = request.auth.credentials.model
+  const myImages = await me.getImages()
+  if (!myImages) {
+    let error = new Error('No images for this user found')
+    throw Boom.boomify(error, { statusCode: 400 })
+  }
 
-        const myImages = await me.getImages()
-
-        return {
-          msg: 'Success! These are all my posted images',
-          myImages
-        }
+  return {
+    msg: 'Success! These are all my posted images',
+    myImages
+  }
+}catch (e) {
+  return h.response('Error')
+}
       }
     }
   }
